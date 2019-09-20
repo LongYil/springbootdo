992bf1b9cdd28af87c21ec96b64042febd75c03f
li-yin-long <18312884980@163.com>
2019-09-20 16:35:27 +0800

    第二次提交，已完成mybatisdo和redisdo。

---
+++ redisdo/src/main/java/com/longdatech/redisdo/Redisdo_Application.java
@@ -0,0 +1,39 @@
+package com.longdatech.redisdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@SpringBootApplication
+public class Redisdo_Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Redisdo_Application.class,args);
+    }
+
+    /**
+     * jedis
+     */
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6379);
+        configuration.setDatabase(0);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(JedisConnectionFactory jedisConnectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        return redisTemplate;
+    }
+
+}
