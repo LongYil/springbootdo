767b667f19ed3172a6a58297d640cdbe6ada3bc9
li-yin-long <18312884980@163.com>
2019-09-25 17:44:26 +0800

    第二次提交，新增swagger模块。

--- swaggerdo/src/main/java/com/longdatech/swaggerdo/controller/UserController.java
+++ swaggerdo/src/main/java/com/longdatech/swaggerdo/controller/UserController.java
@@ -1,19 +1,28 @@
 package com.longdatech.swaggerdo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Random;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/test")
-    public String test() throws Exception{
+    @PostMapping("/test")
+    public String test(HttpServletRequest request) throws Exception{
+        
         Random random = new Random();
         Thread.sleep(random.nextInt(10) * 1000);
+        log.info("测试swaggerdo");
         return "hello,world" + Thread.currentThread().getName();
     }
 
