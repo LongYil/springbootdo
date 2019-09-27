7c2d445d0d0e89a12022db9bc11e89311c9094d4
li-yin-long <18312884980@163.com>
2019-09-27 15:30:17 +0800

    第二次提交，新增swagger模块。

--- swaggerdo/src/main/java/com/longdatech/swaggerdo/controller/UserController.java
+++ swaggerdo/src/main/java/com/longdatech/swaggerdo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.longdatech.swaggerdo.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,21 +11,31 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @PostMapping("/test")
-    public String test(HttpServletRequest request) throws Exception{
+    private Set<String> set = new HashSet<>();
 
+    @PostMapping("/test")
+    public String test() throws Exception{
+
+        set.add(Thread.currentThread().getName());
         Random random = new Random();
-        Thread.sleep(random.nextInt(10) * 1000);
+        Thread.sleep(random.nextInt(10) * 100);
         log.info("测试swaggerdo");
         return "hello,world" + Thread.currentThread().getName();
     }
 
+    @GetMapping("/getThread")
+    public String getThread(){
+        System.out.println("线程数：" + set.size());
+        return "" + set.size();
+    }
 
 }
